bb7f43cdd03c9bd38587cd586cb73630af498f1b
Gee Chai <fc00004@gmail.com>
2023-07-27 23:52:34 +0900

    feat: calculate sum & average

--- sum_numbers/src/main.rs
+++ sum_numbers/src/main.rs
@@ -1,10 +1,32 @@
-fn main() {
-    let array = [10, 20, 30, 40, 50, 60, 70];
-    let mut sum:i64 = 0;
-    println!("This program finds the sum of the array!");
+use std::io;
 
-    for i in 0..array.len(){
-        sum+= array[i];
+fn kahan_sum(numbers: &[f64]) -> f64 {
+    let mut sum = 0.0;
+    let mut c = 0.0;
+    for &num in numbers{
+        let y = num - c;
+        let t = sum + y;
+        c = (t - sum) - y;
+        sum = t;
     }
-    println!("The sum of the array of numbers {}", sum);
+    sum
+}
+
+fn main() {
+    println!("This program finds the sum and average of a list of numbers!");
+    println!("Enter a list of numbers separated by spaces: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let numbers: Vec<f64> = input
+        .split_whitespace()
+        .filter_map(|s| s.parse::<f64>().ok())
+        .collect();
+    
+    let sum = kahan_sum(&numbers);
+    let average = sum / (numbers.len() as f64);
+
+    println!("Numbers: {:?}", numbers);
+    println!("Sum: {:.2}", sum);
+    println!("Average {:.2}", average);
 }
