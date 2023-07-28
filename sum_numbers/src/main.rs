89430a51026a57abe3027abb048730f43f758d71
Gee Chai <fc00004@gmail.com>
2023-07-28 22:48:05 +0900

    feat: standard deviation calculated

--- sum_numbers/src/main.rs
+++ sum_numbers/src/main.rs
@@ -12,8 +12,23 @@ fn kahan_sum(numbers: &[f64]) -> f64 {
     sum
 }
 
+fn calculate_mean(numbers: &[f64]) -> f64 {
+    let sum = kahan_sum(numbers);
+    sum / (numbers.len() as f64)
+}
+
+fn calculate_standard_deviation(numbers: &[f64], mean:f64) -> f64 {
+    let mut sum_squared_diff = 0.0;
+    for &num in numbers {
+        let diff = num - mean;
+        sum_squared_diff += diff * diff
+    }
+    let mean_squared_diff = sum_squared_diff / (numbers.len() as f64);
+    mean_squared_diff.sqrt()
+}
+
 fn main() {
-    println!("This program finds the sum and average of a list of numbers!");
+    println!("This program finds the sum, average, and standard deviation of a list of numbers!");
     println!("Enter a list of numbers separated by spaces: ");
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
@@ -24,9 +39,12 @@ fn main() {
         .collect();
     
     let sum = kahan_sum(&numbers);
-    let average = sum / (numbers.len() as f64);
+    let mean = calculate_mean(&numbers);
+    let standard_deviation = calculate_standard_deviation(&numbers, mean);
+
 
     println!("Numbers: {:?}", numbers);
     println!("Sum: {:.2}", sum);
-    println!("Average {:.2}", average);
+    println!("Average {:.2}", mean);
+    println!("Standard Deviation {:.2}", standard_deviation);
 }
